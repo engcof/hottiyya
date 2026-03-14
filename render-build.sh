92418ac64ca87afe8365c9d332371b07ec109685
engcof <engkhalo70@gmail.com>
2026-03-14 23:23:20 +0300

    Update build script to include postgresql-client

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,11 @@
+
 #!/usr/bin/env bash
 # إنهاء السكربت عند حدوث أي خطأ
 set -o errexit
 
-# 1. تثبيت Ghostscript (محرك الضغط)
-# ملاحظة: في بيئة Render قد لا تحتاج لـ sudo
-apt-get update && apt-get install -y ghostscript
+# 1. تحديث المستودعات وتثبيت الأدوات اللازمة
+# أضفنا postgresql-client لضمان نجاح عملية الاستيراد (Import)
+apt-get update && apt-get install -y ghostscript postgresql-client
 
 # 2. تحديث pip وتثبيت مكتبات بايثون
 pip install --upgrade pip
